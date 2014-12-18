58ffc500862e112ee00c23ab091852b951d552e3
Dolly <dollyg@INDOLLYG.corporate.thoughtworks.com>
2014-12-18 13:30:00 +0530

    add new topic ui under construction

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var topic_module = require('../own_modules/topic_module.js').init('./data/adda.db');
+var new_topic_module = require('../own_modules/new_topic_module.js').init('./data/adda.db');
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Adda' });
 });
 
 router.get('/topic/:id', function(req, res) {
@@ -12,5 +12,19 @@ router.get('/topic/:id', function(req, res) {
   		res.render('topic',topic);
 	});
 });
+
+router.get('/topics',function(req, res){
+	res.render('topics');
+})
+
+router.post('/topics',function(req, res){
+	var new_topic = req.body;
+	// new_topic.email = req.session.user
+	new_topic.email = "ankur@ex.com"
+	console.log("~~~",new_topic);
+	new_topic_module.add_new_topic(new_topic,function(){
+		
+	})
+})
 
 module.exports = router;
