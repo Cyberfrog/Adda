5414b0d431db510e6852b98a3e1c37ca250dc7c0
Cyberfrog <gauravd@INGAURAVD.corporate.thoughtworks.com>
2014-12-18 10:43:15 +0530

    topic  raw jade file created and routes established

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var topic_module = require('../own_modules/topic_module.js').init('./data/adda.db');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/topic/:id', function(req, res) {
+	topic_module.get_topic_summary(req.params.id,function(err,topic){
+  		res.render('topic',topic);
+	});
 });
 
 module.exports = router;
