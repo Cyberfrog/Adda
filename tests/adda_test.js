3c44835def5532a70dea2551a2ce9e2525097e21
parmatma <pyadav.it@gmail.com>
2014-12-27 17:29:09 +0530

    testing

--- tests/adda_test.js
+++ tests/adda_test.js
@@ -22,6 +22,7 @@ describe('adda_records',function(){
 				assert.equal(topic.close_time,undefined);
 				assert.equal(topic.admin,'dolly');
 				assert.lengthOf(topic.comments,2);
+				assert.equal(2,3);
 				done();	
 			});
 		});
